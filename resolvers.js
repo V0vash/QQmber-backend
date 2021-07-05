774146b87d3058e8f8ab9e2b411eebfd2c655cae
Vladimir <avevanguard@gmail.com>
2021-07-05 21:59:51 +0300

    + GET_INFINITE_SCROLL_POSTS

--- resolvers.js
+++ resolvers.js
@@ -1,6 +1,8 @@
 module.exports = {
     Query: {
+
       getUser: () => null,
+
       getPosts: async (_, args, { Post }) => {
         const posts = await Post.find({})
           .sort({ createdDate: "desc" })
@@ -9,9 +11,38 @@ module.exports = {
             model: "User"
           });
         return posts;
+      },
+
+      infiniteScrollPosts: async (_, { pageNum, pageSize }, { Post }) => {
+        let posts;
+        if (pageNum === 1) {
+          posts = await Post.find({})
+            .sort({ createdDate: "desc" })
+            .populate({
+              path: "createdBy",
+              model: "User"
+            })
+            .limit(pageSize);
+        } else {
+          // If page number is greater than one, figure out how many documents to skip
+          const skips = pageSize * (pageNum - 1);
+          posts = await Post.find({})
+            .sort({ createdDate: "desc" })
+            .populate({
+              path: "createdBy",
+              model: "User"
+            })
+            .skip(skips)
+            .limit(pageSize);
+        }
+        const totalDocs = await Post.countDocuments();
+        const hasMore = totalDocs > pageSize * pageNum;
+        return { posts, hasMore };
       }
+
     },
     Mutation: {
+      
       signupUser: async (_, { username, email, password }, { User }) => {
         const user = await User.findOne({ username });
         if (user) {
@@ -24,6 +55,7 @@ module.exports = {
         }).save();
         return newUser;
       },
+      
       addPost: async (
         _,
         { title, imageUrl, categories, description, creatorId },
